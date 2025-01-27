1f3c104db66b58ffadc6676eda24dba4f6ec5bed
Utsav Katharotiya <utsavkatharotiya0712@gmail.com>
2025-01-27 12:23:33 +0530

    Initial commit

---
+++ Tailwind CSS/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'blue': '#0000ff',  
+      'violet': '#8f00ff',
+      'white': '#ffffff',
+      'lightblue': '#0099cc',
+      'gray': '#504f4f',
+      'orange': '#f47a00',
+      'black': '#000000',
+      'lightorange': '#ffb668'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
