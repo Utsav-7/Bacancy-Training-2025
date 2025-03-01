266054f2d054324c4f788c2844c1b2b8a62d14e0
Utsav Katharotiya <utsavkatharotiya0712@gmail.com>
2025-03-01 21:22:03 +0530

    SQL Day-1 Task Completed

--- SQL/SQL_Day-1/SQL_Practical_Task.sql
+++ SQL/SQL_Day-1/SQL_Practical_Task.sql
@@ -6,6 +6,12 @@ ON MS.PlayerID = P.PlayerID
 INNER JOIN Tournaments AS T
 ON MS.TournamentID = T.TournamentID;
 
+
+-- 2. Implement Keys: Use PKs for unique identification and FKs to link match results.
+EXEC sp_pkeys 'Players';
+EXEC sp_pkeys 'Tournaments';
+
+
 -- 3. Create Views: Design views to track tournament results and player performance.
 CREATE VIEW PlayerPerformance AS
 SELECT P.Name, P.Sport, T.Name AS Tournament_Name, T.Date, T.Location, MS.Score
@@ -16,6 +22,7 @@ INNER JOIN Tournaments AS T
 ON MS.TournamentID = T.TournamentID;
 
 SELECT * FROM PlayerPerformance;
+
 
 -- 4. SQL Functions: Develop a function to compute the average score for each player.
 CREATE FUNCTION fn_AvgScoreOfAllPlayers()
@@ -34,18 +41,22 @@ RETURN
 
 SELECT * FROM dbo.fn_AvgScoreOfAllPlayers();
 
+
 -- 5. Stored Procedures: Write a stored procedure to record match results and update player rankings.
 CREATE PROCEDURE PlayersRanking
 AS
 BEGIN
-	SELECT P.Name, P.Name, AVG(MR.Score) AS Score
-	FROM Players P
-	INNER JOIN MatchResults AS MR
-	ON P.PlayerID = MR.PlayerID
-	ORDER BY Score DESC;
+    SELECT 
+        P.Name, 
+        AVG(MR.Score) AS AverageScore
+    FROM Players P
+    INNER JOIN MatchResults MR
+        ON P.PlayerID = MR.PlayerID
+    GROUP BY P.Name
+    ORDER BY AverageScore DESC;
 END;
-
 EXEC PlayersRanking;
+
 
 -- 6. SQL Joins: Write JOIN queries to combine data from Players, Tournaments, and MatchResults for complete performance reports.
 SELECT P.Name, P.Contact, P.Sport, T.Name AS Tournament_Name, T.Date, T.Location, MS.Score
@@ -54,6 +65,7 @@ INNER JOIN Players AS P
 ON MS.PlayerID = P.PlayerID
 INNER JOIN Tournaments AS T
 ON MS.TournamentID = T.TournamentID;
+
 
 -- 7. Indexes: Create indexes on PlayerID, TournamentID, and Date to improve query performance.
 CREATE INDEX IX_Players ON Players (PlayerID);
