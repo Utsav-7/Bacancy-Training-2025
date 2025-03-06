49187b24749eb3939ca4bf613eb4283fecf969e4
Utsav Katharotiya <utsavkatharotiya0712@gmail.com>
2025-03-06 17:00:11 +0530

    Assignment-2 Submitted

---
+++ EF - Core/EFCore_Day-1/Assignment-2/DBContext_DI/Data/AppDBContext.cs
@@ -0,0 +1,12 @@
+﻿using DBContext_DI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBContext_DI.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options){ }
+
+        public DbSet<Sport> Sports { get; set; }
+    }
+}
---
+++ EF - Core/EFCore_Day-1/Assignment-2/DBContext_DI/Models/Sport.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DBContext_DI.Models
+{
+    public class Sport
+    {
+        [Key]
+        public int SportId { get; set; }
+        [ForeignKey("Studens")]
+        public int StudentId { get; set; }
+        public string SportName { get; set; }
+    }
+}
