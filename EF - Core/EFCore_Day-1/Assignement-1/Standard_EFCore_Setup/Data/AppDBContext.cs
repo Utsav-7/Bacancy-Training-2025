a20ef22e067e90bc7454a7add0b49e23b34039af
Utsav Katharotiya <utsavkatharotiya0712@gmail.com>
2025-03-06 17:22:40 +0530

    Standard_EFCore_Setup completed

---
+++ EF - Core/EFCore_Day-1/Assignement-1/Standard_EFCore_Setup/Data/AppDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Standard_EFCore_Setup.Models;
+
+namespace Standard_EFCore_Setup.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {   
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ EF - Core/EFCore_Day-1/Assignement-1/Standard_EFCore_Setup/Models/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace Standard_EFCore_Setup.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string Name { get; set; }
+    }
+}
